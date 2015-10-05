6a713b341ab9f50e8cc1fa206a1f91f61dccd500
RobinDX <robin9800xt@gmail.com>
2015-10-05 20:19:13 +0800

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r lovewallpaper /usr/share/pyshared
+cp -r lovewallpaper /usr/share/pyshared/lovewallpaper
 cp data/love-wallpaper.png /usr/share/icons
 cp data/love-wallpaper.desktop /usr/share/applications
 cp love-wallpaper /usr/bin
